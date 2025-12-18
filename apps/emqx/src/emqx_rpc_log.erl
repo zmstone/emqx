55f80bec9094cc6502ecb7f256edefb472ad958b
zmstone <zmstone@gmail.com>
2025-12-18 14:50:29 +0100

    feat: throttle log failed_to_connect_server

---
+++ apps/emqx/src/emqx_rpc_log.erl
@@ -0,0 +1,32 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2017-2025 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%--------------------------------------------------------------------
+
+-module(emqx_rpc_log).
+-moduledoc """
+Log callback for gen_rpc 3.5 or newer.
+Throttle the log message failed_to_connect_server.
+""".
+
+-export([init/0, log/4]).
+
+-include("logger.hrl").
+
+-define(META(TYPE), #{tag => "RPC", domain => [gen_rpc, TYPE]}).
+
+-doc "Called during boot sequence to set gen_rpc logger callback.".
+init() ->
+    gen_rpc:set_logger(?MODULE).
+
+-doc """
+Called by gen_rpc_logger.
+NOTE: the call happens after log level is checked.
+e.g. if current logger level is 'error', Level cannot be 'debug'
+""".
+log(Level, Type, Msg, Data) ->
+    case Msg of
+        "failed_to_connect_server" ->
+            ?SLOG_THROTTLE(Level, Data#{msg => failed_to_connect_server}, ?META(Type));
+        _ ->
+            ?SLOG(Level, Data#{msg => Msg}, ?META(Type))
+    end.
